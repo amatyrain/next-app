62547b3167041472c9945e64e2134e50dd5f8c23
amaty <nwhfd.2022@gmail.com>
2022-07-09 16:44:50 +0900

    update

--- components/decorder.tsx
+++ components/decorder.tsx
@@ -1,19 +1,14 @@
 import styles from '../styles/Home.module.css'
-import Head from 'next/head'
+import { useEffect, useMemo, useState } from "react";
 
 export default function Decorder() {
+  const [url, setUrl] = useState<string>("");
 
   function decord() {
     var ta = document.getElementById("ta") as HTMLInputElement | null;
-    var rta = document.getElementById("rta") as HTMLInputElement | null;
-    var url = '';
+    var url_base = ta !== null ? decodeURIComponent(ta.value) : '';
 
-    if (ta !== null) {
-      url = decodeURIComponent(ta.value);
-    }
-    if (rta !== null) {
-      rta.textContent = url;
-    }
+    setUrl(url_base)
   }
 
   return (
@@ -24,28 +19,26 @@ export default function Decorder() {
       </h1>
       <div>
         <div className="prg">
-          <form id="frm">
-            <div className="encodebox">
-              <div className="procedure">① デコードする文字列を入力します</div>
-              <div id="textarea" className="encodetargetbox">
-                <div className="textarea_cover">
-                  <textarea id="ta" className="encodebox" name="target_encode" cols={34} rows={5}></textarea>
-                </div>
+          <div className="encodebox">
+            <div className="procedure">① デコードする文字列を入力します</div>
+            <div id="textarea" className="encodetargetbox">
+              <div className="textarea_cover">
+                <textarea id="ta" className="encodebox" name="target_encode" cols={34} rows={5}></textarea>
               </div>
-              <div className="clearboth"></div>
-              <span className="procedure">② 下記ボタンをクリックします</span>
-              <div>
-                <input className="exebox encodeexebox" onClick={decord} type="submit" value="変換する" />
-              </div>
-              <div className="procedure">③ デコード後の文字列が表示されます</div>
-              <div id="result_area" className="encoderesultbox">
-                <div id="result_area_cover" className="textarea_cover textarea_result_before">
-                  <textarea id="rta" className="encodebox" name="result_encode" cols={34} rows={5}></textarea>
-                </div>
-              </div>
-              <div className="clearboth"></div>
             </div>
-          </form>
+            <div className="clearboth"></div>
+            <span className="procedure">② 下記ボタンをクリックします</span>
+            <div>
+              <button className="exebox encodeexebox" onClick={decord}>変換する</button>
+            </div>
+            <div className="procedure">③ デコード後の文字列が表示されます</div>
+            <div id="result_area" className="encoderesultbox">
+              <div id="result_area_cover" className="textarea_cover textarea_result_before">
+                <textarea id="rta" className="encodebox" name="result_encode" value={url} cols={34} rows={5}></textarea>
+              </div>
+            </div>
+            <div className="clearboth"></div>
+          </div>
         </div>
       </div>
     </main>
