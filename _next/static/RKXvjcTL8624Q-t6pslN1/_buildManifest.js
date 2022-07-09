b1e539007d9b2338d762729a5e2b7786733c8ab9
amatyrain <amatyrain@users.noreply.github.com>
2022-07-09 07:45:42 +0000

    deploy: 62547b3167041472c9945e64e2134e50dd5f8c23

---
+++ _next/static/RKXvjcTL8624Q-t6pslN1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8b854e8bf320d34f.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/decorder":["static/chunks/pages/decorder-8d8ec4f8f2361868.js"],sortedPages:["/","/_app","/_error","/decorder"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
