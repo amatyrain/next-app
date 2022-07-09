797d6b9bd63c21116e3449fe313aaa090bada61c
amatyrain <amatyrain@users.noreply.github.com>
2022-07-09 05:53:15 +0000

    deploy: 187447b258fbf7df7d3053fc402ac5eb3209c3ca

---
+++ _next/static/B5-KB1f4BuHr2nC4lRZds/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8b854e8bf320d34f.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/decorder":["static/chunks/pages/decorder-bf0e95f68cc2fe98.js"],sortedPages:["/","/_app","/_error","/decorder"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/H67l0Rkz6GTtx0Z1zLKl5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8b854e8bf320d34f.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/decorder":["static/chunks/pages/decorder-fd321af1c2b243f2.js"],sortedPages:["/","/_app","/_error","/decorder"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
